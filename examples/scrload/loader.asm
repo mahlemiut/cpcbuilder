eb08d40902fc1284777cc9783be972b0c6105352
mahlemiut <bsr@xnet.co.nz>
2015-07-20 19:46:16 +1200

    Added a simple example project, which simply loads screen data from a disk file to the screen.

---
+++ examples/scrload/loader.asm
@@ -0,0 +1,51 @@
+; Screen loading test app.
+
+	org &8000
+
+.start:
+	; enter mode 2 (640x200)
+	ld a,2
+	call &bc0e   ; SCR SET MODE
+
+	; open file
+	ld b,10  ; filename length
+	ld hl,.filename  ; filename
+	ld de,.buffer  ; buffer
+	call &bc77  ; CAS IN OPEN
+	call z,.openfail  ; display fail message if open fails
+	
+	; load file contents into RAM
+	ld hl,&c000
+	call &bc83  ; CAS IN DIRECT
+	call z,.openfail
+	
+	; close file
+	call &bc7a  ; CAS IN CLOSE
+
+	; and exit
+	jr .exit
+
+.openfail:
+	ld hl,.msg_openfail
+.txtloop:
+	ld a,(hl)
+	cp 0
+	jr z,.txtdone
+	call &bb5a  ; TXT OUTPUT
+	inc hl
+	jr .txtloop
+.txtdone:
+	ret
+	
+.exit:
+	ret
+
+.msg_openfail:
+	defb "Unable to open screen data."
+	defb 13,10,0
+.filename:
+	defb "shock2.bin"
+	defb 0
+.buffer:
+	defs 2048
+	; will be used for file buffer by the BIOS
